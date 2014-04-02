51e19940c399eb962e261cae83f51a3d00acfa57
rasmusolofzon <rasmus.olofzon@gmail.com>
2014-04-02 10:32:34 +0200

    Början till labben. Första delen, som vi gjorde under förra labbtillfället

--- eda095_lab3/src/eda095_lab3/EchoTCP1.java
+++ eda095_lab3/src/eda095_lab3/EchoTCP1.java
@@ -1,6 +1,8 @@
 package eda095_lab3;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,17 +12,39 @@ public class EchoTCP1 {
 	public EchoTCP1() {
 		try {
 			ss = new ServerSocket(30000);
+			acceptClients();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	public static void main(String[] args) {
+		new EchoTCP1();
+	}
+	
 	public void acceptClients() throws IOException {
 		Socket socka = null;
 		
 		while ((socka = ss.accept()) != null) {
+			System.out.println(socka.getInetAddress());
 			
+			InputStream in = socka.getInputStream();
+			OutputStream out = socka.getOutputStream();
+			
+			int read;
+			StringBuilder sb = new StringBuilder();
+			
+			while ((read = in.read()) != -1) {
+				if (read != 13) {
+					sb.append(read);
+				} else {
+					System.out.println(sb.toString());
+					out.write(sb.toString().getBytes());
+					out.flush();
+					sb.setLength(0);
+				}
+			}
 		}
 		
 	}
